b4bc0fa4faddc401d9deddba29f63f7f6dfc5fb3
Just-so-so-czl <2686059859@qq.com>
2026-04-15 21:46:34 +0800

    init

---
+++ src/main/java/com/czl/teamupbackend/TeamUpBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.czl.teamupbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamUpBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeamUpBackEndApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/czl/teamupbackend/TeamUpBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.czl.teamupbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamUpBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
